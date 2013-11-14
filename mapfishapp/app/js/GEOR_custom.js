4e0ed46d895f608affc1fad1b5714418bb0dbc38
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2013-11-14 08:56:41 +0100

    ROLES_FOR_EDIT defaults to [ROLE_ADMINISTRATOR]

--- mapfishapp/app/js/GEOR_custom.js
+++ mapfishapp/app/js/GEOR_custom.js
@@ -505,6 +505,14 @@ GEOR.custom = {
     //ROLES_FOR_PRINTER: [],
 
     /**
+     * Constant: ROLES_FOR_EDIT
+     * {Array} roles required for the edit functions to show up
+     * Empty array means the module is available for everyone
+     * Defaults to ['ROLE_ADMINISTRATOR']
+     */
+    //ROLES_FOR_EDIT: ['ROLE_ADMINISTRATOR'],
+
+    /**
      * Constant: PRINT_LAYOUTS_ACL
      * {Object} roles required for each print layout
      * Empty array means "layout is available for everyone"
