b875161e187c9db3f6656b8af19fe941aa43be51
François Van Der Biest <francois.vanderbiest@gmail.com>
2017-01-22 14:49:50 +0100

    remove files already provided by datadir

--- analytics/js/app/config.js
+++
@@ -1,7 +0,0 @@
-GEOR = {
-    config: {
-        HEADER_HEIGHT: @shared.header.height@,
-        OGC_STATISTICS: @shared.ogc.statistics.activated@,
-        DOWNLOAD_FORM: @shared.download_form.activated@
-    }
-};
